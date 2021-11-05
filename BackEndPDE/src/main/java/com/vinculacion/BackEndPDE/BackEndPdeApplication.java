5d23f57214d7b3b0642e39852ca26ef469b1c2fe
Richard Gualancañay <51199793+RichardG20105@users.noreply.github.com>
2021-11-05 08:48:25 -0500

    Add files via upload
    Inicialización del Back End

---
+++ BackEndPDE/src/main/java/com/vinculacion/BackEndPDE/BackEndPdeApplication.java
@@ -0,0 +1,13 @@
+package com.vinculacion.BackEndPDE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEndPdeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackEndPdeApplication.class, args);
+	}
+
+}
